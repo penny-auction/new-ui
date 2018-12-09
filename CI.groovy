9ec788bae0843e18d5b5813b6e2609b1beddfb8a
Bogdan Panchuk <bogdan.panchuck@intapp.com>
2018-12-10 00:43:42 +0200

    Added UI build steps.

--- CI.groovy
+++ CI.groovy
@@ -1,13 +1,20 @@
-node("jenkins-slave") {
-    container("ci-cd-agent") {
-        stage("Checkout sources") {
+node "jenkins-slave", {
+    container "ci-cd-agent", {
+        stage "Checkout sources", {
             checkout scm
         }
 
-        docker.withRegistry("", "javatechnologies") {
-            docker
-                .build("javatechnologies/penny-auction-ui:dev")
-                .push()
+        stage "Build application", {
+            sh "npm i -g yarn yarn"
+            sh "yarn build"
+        }
+
+        stage "Build and push Docker image"), {
+            docker.withRegistry "", "javatechnologies", {
+                docker
+                    .build("javatechnologies/penny-auction-ui:dev")
+                    .push()
+            }
         }
     }
 }
